06267fae746f9313fb5f543916e4a89320a315b5
wutsi <herve.tchepannou@wutsi.com>
2022-01-20 14:19:29 -0500

    update

--- lib/viewer.dart
+++ lib/viewer.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:sdui/sdui.dart';
 
 class SDUIViewerPage extends StatefulWidget {
